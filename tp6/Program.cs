f82b078bc95028bac2e7d2ec80baa22b8485fd9a
Santiago Toledo <santi.nt2@gmail.com>
2019-05-16 18:02:45 -0300

    separe los puntos

--- tp6/Program.cs
+++ tp6/Program.cs
@@ -13,8 +13,6 @@ namespace tp6
             int n = 1893;
             Console.WriteLine($"{n} invertido: {Invertir(n)}");
 
-            Calculadora();
-
         }
 
         public static int Invertir(int n)
@@ -32,53 +30,6 @@ namespace tp6
                 }
                 return inv;
             }
-        }
-
-        public static void Calculadora()
-        {
-            int a, b, res = 0, menu = 5;
-            while (menu != 0)
-            {
-                Console.WriteLine("Calculadora: \n 1-Sumar \n 2-Restar \n 3-Multiplicar \n 4-Dividir");
-                menu = Convert.ToInt32(Console.ReadLine());
-         
-                Console.WriteLine("Inrgrese el primer numero: ");
-                a = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("Inrgrese el segundo numero: ");
-                b = Convert.ToInt32(Console.ReadLine());
-
-                switch (menu)
-                {
-                    case 1:
-                        res = a + b;
-                        break;
-                    case 2:
-                        res = a - b;
-                        break;
-                    case 3:
-                        res = a * b;
-                        break;
-                    case 4:
-                        res = a - b;
-                        break;
-                    default:
-                        Console.WriteLine("No se ingreso una opcion valida");
-                        break;
-                }
-
-                Console.WriteLine("\nResultado: {0}", res);
-                Console.WriteLine("\nIngrese 5 para realizar otra operacion, 0 para salir");
-                menu = Convert.ToInt32(Console.ReadLine());
-            }  
-
-        }
-
-        public static void Cadenas()
-        {
-            string a, b; 
-            Console.WriteLine("Ingrese una cadena: ");
-            a = Console.ReadLine();
-
         }
     }
 }
