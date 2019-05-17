3a27d2c0de3e840eaa511b78dce1113b4fe1fb7f
SantiagoToledo <santi.nt2@gmail.com>
2019-05-16 21:25:33 -0300

    same

---
+++ tp6/Ejercicio1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 1893;
+            Console.WriteLine("Ingrese un numero: ");
+            n = Console.Read();
+
+            Console.WriteLine($"{n} invertido: {Invertir(n)}");
+
+        }
+
+        public static int Invertir(int n)
+        {
+            int inv = 0;
+            if (n == 0)
+            {
+                return n;
+            }
+            else
+            {
+                while (n > 0)
+                {
+                    inv = inv * 10 + (n % 10);
+                    n = n / 10;
+                }
+                return inv;
+            }
+        }
+    }
+}
